e9d6bace57a105253b09694c0cb69cd61da86df4
sagar chhatrola <sagar.chhatrola77@gmail.com>
2021-03-28 12:57:42 +0530

    inital commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Player player;
+    private Vector3 lastPosition;
+    private float distance;
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        lastPosition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distance = player.transform.position.x - lastPosition.x;
+        transform.position = new Vector3(
+            transform.position.x + distance,
+            transform.position.y,
+            transform.position.z
+        );
+        lastPosition = player.transform.position;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(speed,rigidBody.velocity.y);
+    }
+}
